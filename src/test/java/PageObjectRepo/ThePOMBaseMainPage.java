371e8fcbf5592ede3a7d87855e7d3d7c5be8cf40
andresjoeam <akademicandrew@gmail.com>
2024-08-06 22:28:09 -0400

    New implementations before using TestNG execution

--- src/test/java/PageObjectRepo/ThePOMBaseMainPage.java
+++ src/test/java/PageObjectRepo/ThePOMBaseMainPage.java
@@ -5,27 +5,21 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class ThePOMBaseMainPage {
-    WebDriver driver;
+    private WebDriver driver;
     //By theLogo = By.xpath("//span[@class='hmsprite logo']");
     By theLogo = By.cssSelector(".css-bqykk2 > .css-zc1vnn");
     By wishListButton_By = By.cssSelector(".css-5tv7te > .css-rlfxqc .css-zc1vnn");
 
-
-    public ThePOMBaseMainPage(WebDriver driver)
-    {
-        this.driver=driver;
+    public ThePOMBaseMainPage(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public WebElement forLogo()
-    {
+    public WebElement forLogo() {
         return driver.findElement(theLogo);
     }
 
-    public WebElement wishListButton()
-    {
+    public WebElement wishListButton() {
         return driver.findElement(wishListButton_By);
     }
-
-
 }
 
